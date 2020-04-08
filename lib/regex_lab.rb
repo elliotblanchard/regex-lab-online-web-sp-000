f61be6cc232a8d6bb1024dba3f59732d58522fa2
Elliot Blanchard <public@elliotblanchard.com>
2020-04-08 01:50:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if text.scan(/\A([A-Z]\D*[.?!])\z/).length > 0
+  if text.scan(/\D?\d{3}\D?\d{3}\D?\d{4}\b/).length > 0
     true
   else
     false
