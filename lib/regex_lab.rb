ba9a7e292434ee530edcbda6fb870bc2144ec4f6
Elliot Blanchard <public@elliotblanchard.com>
2020-04-08 00:40:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,19 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if word.scan(/\Aun\zing/i).length > 0
+    true
+  else
+    false
+  end
 end
 
 def words_five_letters_long(text)
-
+  if word.scan(/\A\w{5}\Z/i).length > 0
+    true
+  else
+    false
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
